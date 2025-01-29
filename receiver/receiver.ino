940aebe06bde69d40b81d141cf9a9ff84d6a70af
KryoZek <KryoZek@protonmail.com>
2025-01-28 20:19:13 -0400

    [DEV] Ver 0.a ---- Created basic setup.

---
+++ receiver/receiver.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+// CE: Chip Enable        -> D9
+// CSN: Chip Select Not   -> D10
+// IRQ: Interrupt         -> D6
+
+// MISO: MiSo             -> D12
+// MOSI: MoSi             -> D11
+// SCK: Serial Clock      -> D13
+
+#define D9    9
+#define D10   10
+#define IRQ   6
+
+#define MISO  12
+#define MOSI  11
+#define SCK   13
+
+
+RF24 radio(D9, D10);  // CE, CSN
+
+const byte rf_addr [][6] = {"00001", "00002"};
+bool rf_active = 0;
+unsigned long msg_sent = 0;
+unsigned long msg_recv = 0;
+
+void setup() {
+
+  radio.begin();
+  
+  // We'll utilize 00002 as our Writing Pipe
+  radio.openWritingPipe(rf_addr[1]);
+  // We'll utilize 00001 as our Reading Pipe
+  radio.openReadingPipe(1, rf_addr[0]);
+
+  // ??
+  radio.setPALevel(RF24_PA_MIN);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
