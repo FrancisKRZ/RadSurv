96d3f66d18bab98d0c12c5210b6570fb29b6884f
FrancisKRZ <krzfrancesc@gmail.com>
2025-02-17 19:19:38 -0400

    [WIP] Deleted server's List and axon source files, will be replaced by a localized server.py to manage incoming connections from transmitters for localized storage, taking advantage of OS' bit protection(s)

--- server/server.py
+++ server/server.py
@@ -2,3 +2,12 @@
 # An NRF24 module will be loaded into the machine, capture the varios addr signals
 # Append the signal readouts to a buffer then write after a certain size and/or time
 
+# Sys for files, socket for wireless connection, time for managing file saving parametrization
+import sys
+import socket
+import time
+
+# Multi-Threaded for Server and Radio operations
+from threading import Lock, Thread
+
+
