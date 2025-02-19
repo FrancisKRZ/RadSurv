18a229ae6123880eb1970f61f76da35e692538cc
FrancisKRZ <krzfrancesc@gmail.com>
2025-02-19 11:14:19 -0400

    [WIP] Added read pointer to SharedQueue, to avoid inverted queue structure

--- server/rf_to_server.py
+++ server/rf_to_server.py
@@ -40,6 +40,7 @@ class SharedQueue:
         
         self.depth = depth                          # Queue depth limit
         self.count = 0                              # Current item count, starts at 0
+        self.rd_ptr = 0
         # All flags are type bool
         # Empty flag , Full flag
         # Almost Empty flag  , Almost Full flag 
@@ -47,18 +48,29 @@ class SharedQueue:
         # Queue
         self.queue = [None] * self.depth
 
+    # [Mutex] Acquire and Release mutex during use (caller function) instead of implementation
     def write_data(self, data):
 
         try:
             if self.get_Full_flag() is False:
                 self.queue[count] = data
-                count = count + 1
+                self.count = self.count + 1
         except:
-            # CR - Mutex
-            mutex.acquire()
             print("Failed SharedQueue write_data")
             logger.error("Error: Device %s Failed SharedQueue write_data", socket.gethostbyname())
-            mutex.release()
+
+    # [Mutex]
+    def read_data(self):
+
+        try:
+            if self.get_Empty_flag is False:
+                self.count = self.count - 1      # Update size counter
+                self.rd_ptr = rd_ptr + 1         # Update read pointer prior to return
+                return SharedQueue[rd_ptr-1]     # return the original pointed Queue rd
+        except:
+            print("Failed to read data")
+            logger.error("Error: Device %s Failed ShareQueue read_data", socket.gethostbyname())
+
 
     # Get Queue Buffer status flags
     def get_Full_flag(self):
